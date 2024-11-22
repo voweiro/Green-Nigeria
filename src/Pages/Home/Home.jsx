7b0cd908d795ec0c80ed6f95f179197c04ed53fc
voweiro <ajenaghonorevoweiro@gmail.com>
2024-11-22 20:18:54 +0100

    change climate folder

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,13 +1,12 @@
 import Hero from "../../Components/Hero/hero"
 
-import Climate from '../../Components/Climate/Climate'
 import StatsAndProcess from '../../Components/Stats/Stats'
 
 function Home() {
   return (
     <div>
      <Hero/>
-    <Climate/>
+    
     <StatsAndProcess/>
     </div>
     
