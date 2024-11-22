320fd6970417b677a590ac6bebfaf045716f154d
Ekezie Uchenna <105205124+Uchechukwu-Ekezie@users.noreply.github.com>
2024-11-22 16:01:18 +0100

    welcome to green

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RootLayout from "./Layout/RootLayout";
+import Home from "./Pages/Home/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div className=''>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <div className="bg-slate-300 h-dvh">
+          <Routes>
+            <Route path="/" element={<RootLayout />}>
+              <Route index element={<Home />} />
+            </Route>
+            {/* <Route path="/about" element={<About />} />
+          <Route path="/services" element={<Services />} />
+          <Route path="/contact" element={<Contact />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,16 @@
+import { Outlet } from 'react-router-dom'
+import Navigation from '../Components/Header/Navigation'
+
+function RootLayout() {
+  return (
+    <div>
+        <Navigation/>
+        <main>
+            <Outlet/>
+        </main>
+
+    </div>
+  )
+}
+
+export default RootLayout
